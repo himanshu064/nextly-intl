3edefc97d645ae22470ecf3840abed79fbb37f5d
Himanshu Kumar <himanshu064@gmail.com>
2024-11-14 17:42:56 +0530

    fixed the bug in language selector dropdown

--- src/components/LocaleSwitcher.js
+++ src/components/LocaleSwitcher.js
@@ -2,14 +2,12 @@ import { useTranslations } from "next-intl";
 import React from "react";
 import LocaleSwitcherSelect from "./LocaleSwitcherSelect";
 import { locales } from "@/config";
-import { getLocaleFromCookie } from "@/lib/cookieUtls";
 
 const LocaleSwitcher = () => {
   const t = useTranslations("LocaleSwitcher");
-  const locale = getLocaleFromCookie() || "en";
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale} label={t("label")}>
+    <LocaleSwitcherSelect label={t("label")}>
       {locales?.map((curr) => (
         <option key={curr} value={curr}>
           {t("locale", { locale: curr })}
--- src/lib/cookieUtls.js
+++ src/lib/cookieUtls.js
@@ -1,6 +1,15 @@
+"use server";
+import { cookies } from "next/headers";
+
 export const getLocaleFromCookie = () => {
   if (typeof document !== "undefined") {
     const match = document.cookie.match(/locale=([^;]+)/);
     return match ? match[1] : "en";
+  } else {
+    const cookieStore = cookies();
+    console.log(cookieStore.get("locale"), "cookieStore");
+
+    const locale = cookieStore.get("locale")?.value || "en";
+    return locale;
   }
 };
